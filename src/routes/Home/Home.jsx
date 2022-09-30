abb701af5666a73396f4050702969ea8d433cade
Adarsh Pratap Singh <adarsh.lrnr@gmail.com>
2022-09-30 19:40:01 +0530

    ➕ adding helmet support

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React, { Fragment } from "react";
-// import { Outlet } from "react-router-dom";
+import { Helmet } from "react-helmet";
+
 import "./home.styles.scss";
 
 export default function Home() {
 	return (
 		<Fragment>
+			<Helmet>
+				<title>Audiophile | Home</title>
+			</Helmet>
 			<div>This is home page</div>
 		</Fragment>
 	);
